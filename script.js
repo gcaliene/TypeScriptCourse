ce06552504efbeacc632e81de6d77825fab197f7
gerson C <gersoncalienes@gmail.com>
2018-04-10 14:09:24 -0400

    complex object and type Alias

--- script.js
+++ script.js
@@ -10,8 +10,24 @@ var myMultiply;
 //myMultiply();
 myMultiply = multiply;
 console.log(myMultiply(5, 2));
+//in a function, the order matters so the naming dont matters
+//in an object, the  naming matters but the order does not
 //objects
 var userData = {
     name: 'Max',
     age: 27
+}; //whole object
+//userData ={ a:'Hello', b:22} wont work because the naming is off
+// complex object
+var complex = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+var complex2 = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
 };
--- script.ts
+++ script.ts
@@ -13,8 +13,31 @@ let myMultiply: (a: number, b:number ) => number;
 myMultiply = multiply;
 console.log(myMultiply(5,2));
 
+//in a function, the order matters so the naming dont matters
+//in an object, the  naming matters but the order does not
+
 //objects
-let userData = {
-  name:'Max',
-  age:27
-}
+var userData:{ name: string, age:number } = {
+    name: 'Max',
+    age: 27
+}; //whole object
+
+//userData ={ a:'Hello', b:22} wont work because the naming is off
+
+// complex object
+let complex: {data:number[], output:(all:boolean)=> number[]}= {
+  data: [100,3.99, 10],
+  output:function(all:boolean): number[]{
+    return this.data;
+  }
+};
+
+//type Alias
+type Complex = {data:number[], output:(all:boolean)=> number[]};
+
+let complex2: Complex= {
+  data: [100,3.99, 10],
+  output:function(all:boolean): number[]{
+    return this.data;
+  }
+};
